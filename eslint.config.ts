ff555a1aec2bc720c3ddcd3ce822573652485e45
Kamiya <kamiya@kamiya.app>
2025-10-02 12:41:27 +0800

    b

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,31 +1,21 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import { defineConfig } from 'eslint/config';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 
+import drizzle from 'eslint-plugin-drizzle';
 import javascript from '@eslint/js';
 import perfectionist from 'eslint-plugin-perfectionist';
 import prettyImport from '@kamiya4047/eslint-plugin-pretty-import';
 import stylistic from '@stylistic/eslint-plugin';
 import typescript from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 export default defineConfig(
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-    ],
+    ignores: ['.next'],
   },
   {
     languageOptions: {
@@ -34,6 +24,9 @@ export default defineConfig(
         projectService: true,
         tsconfigRootDir: __dirname,
       },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
     },
     name: 'parser',
   },
@@ -44,8 +37,25 @@ export default defineConfig(
   perfectionist.configs['recommended-alphabetical'],
   prettyImport.configs.warn,
   {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      drizzle,
+    },
+    rules: {
+      'drizzle/enforce-delete-with-where': [
+        'error',
+        { drizzleObjectName: ['db', 'ctx.db'] },
+      ],
+      'drizzle/enforce-update-with-where': [
+        'error',
+        { drizzleObjectName: ['db', 'ctx.db'] },
+      ],
+    },
+  },
+  {
     name: 'disables',
     rules: {
+      '@typescript-eslint/restrict-template-expressions': 'off',
       'perfectionist/sort-imports': 'off',
       'perfectionist/sort-named-imports': 'off',
     },
---
+++ src/server/db/schema.ts
@@ -0,0 +1,109 @@
+import { index, pgTableCreator, primaryKey } from 'drizzle-orm/pg-core';
+import { relations, sql } from 'drizzle-orm';
+
+import type { AdapterAccount } from 'next-auth/adapters';
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = pgTableCreator((name) => `healthy-diet_${name}`);
+
+export const posts = createTable(
+  'post',
+  (d) => ({
+    createdAt: d
+      .timestamp({ withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    createdById: d
+      .varchar({ length: 255 })
+      .notNull()
+      .references(() => users.id),
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    name: d.varchar({ length: 256 }),
+    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  }),
+  (t) => [
+    index('created_by_idx').on(t.createdById),
+    index('name_idx').on(t.name),
+  ],
+);
+
+export const users = createTable('user', (d) => ({
+  email: d.varchar({ length: 255 }).notNull(),
+  emailVerified: d
+    .timestamp({
+      mode: 'date',
+      withTimezone: true,
+    })
+    .default(sql`CURRENT_TIMESTAMP`),
+  id: d
+    .varchar({ length: 255 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  image: d.varchar({ length: 255 }),
+  name: d.varchar({ length: 255 }),
+}));
+
+export const usersRelations = relations(users, ({ many }) => ({
+  accounts: many(accounts),
+}));
+
+export const accounts = createTable(
+  'account',
+  (d) => ({
+    access_token: d.text(),
+    expires_at: d.integer(),
+    id_token: d.text(),
+    provider: d.varchar({ length: 255 }).notNull(),
+    providerAccountId: d.varchar({ length: 255 }).notNull(),
+    refresh_token: d.text(),
+    scope: d.varchar({ length: 255 }),
+    session_state: d.varchar({ length: 255 }),
+    token_type: d.varchar({ length: 255 }),
+    type: d.varchar({ length: 255 }).$type<AdapterAccount['type']>().notNull(),
+    userId: d
+      .varchar({ length: 255 })
+      .notNull()
+      .references(() => users.id),
+  }),
+  (t) => [
+    primaryKey({ columns: [t.provider, t.providerAccountId] }),
+    index('account_user_id_idx').on(t.userId),
+  ],
+);
+
+export const accountsRelations = relations(accounts, ({ one }) => ({
+  user: one(users, { fields: [accounts.userId], references: [users.id] }),
+}));
+
+export const sessions = createTable(
+  'session',
+  (d) => ({
+    expires: d.timestamp({ mode: 'date', withTimezone: true }).notNull(),
+    sessionToken: d.varchar({ length: 255 }).notNull().primaryKey(),
+    userId: d
+      .varchar({ length: 255 })
+      .notNull()
+      .references(() => users.id),
+  }),
+  (t) => [index('t_user_id_idx').on(t.userId)],
+);
+
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  user: one(users, { fields: [sessions.userId], references: [users.id] }),
+}));
+
+export const verificationTokens = createTable(
+  'verification_token',
+  (d) => ({
+    expires: d.timestamp({ mode: 'date', withTimezone: true }).notNull(),
+    identifier: d.varchar({ length: 255 }).notNull(),
+    token: d.varchar({ length: 255 }).notNull(),
+  }),
+  (t) => [primaryKey({ columns: [t.identifier, t.token] })],
+);
