043ae9c421369e9aaa3d0ab0cc0a575dd72f4095
archie0732 <121162902+archie0732@users.noreply.github.com>
2025-11-27 23:02:24 +0800

    add login logic

--- healthy-diet-api-server/src/main.rs
+++ healthy-diet-api-server/src/main.rs
@@ -9,7 +9,7 @@ async fn main() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "rust_api_server=debug,tower_http=debug".into()),
+                .unwrap_or_else(|_| "healthy_diet_api_server=debug,tower_http=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
--- healthy-diet-api-server/src/router.rs
+++ healthy-diet-api-server/src/router.rs
@@ -2,10 +2,12 @@ use axum::{Router, routing::get};
 
 use tower_http::trace::TraceLayer;
 
-use crate::{common::test::test, nhentai::search::nhentai_search};
+use crate::discord::login::{discord_callback, login_discord};
 
 pub fn create_app() -> Router {
     Router::new()
-        .route("/", async || "Connect Success!")
+        .route("/", get(async || "Connect Success!"))
+        .route("/api/auth/discord/login", get(login_discord))
+        .route("/api/auth/discord/callback", get(discord_callback))
         .layer(TraceLayer::new_for_http())
 }
